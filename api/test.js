b61f53cb47c7b7be25654a504b1813a67f9c8bb1
Muhammad Farrukh Javed <mfarrukhjaved381@gmail.com>
2025-10-08 18:07:57 +0500

    Remove conflicting JS files

--- api/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-  res.json({
-    message: 'Vercel serverless function is working!',
-    timestamp: new Date().toISOString(),
-    method: req.method,
-    url: req.url,
-    headers: req.headers
-  });
-};
